4a6d6f88b461a01721773ff32b1fc10703557d78
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-24 22:09:04 +0330

    contact us - about us

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ from django.views.generic import TemplateView
 app_name = "accounts"
 urlpatterns = [
     path('', TemplateView.as_view(template_name="core/home.html"), name="home"),
+    path('team/about-us/', TemplateView.as_view(template_name="core/aboutus.html"), name="home"),
+    path('contact-us/', TemplateView.as_view(template_name="core/contactus.html"), name="home"),
 ]
