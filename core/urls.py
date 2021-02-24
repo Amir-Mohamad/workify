db2a6051e412e1ac84783ac62944be09ec52bdf6
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-24 22:52:01 +0330

    Update

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 from django.views.generic import TemplateView
 
+
 app_name = "accounts"
 urlpatterns = [
     path('', TemplateView.as_view(template_name="core/home.html"), name="home"),
-    path('team/about-us/', TemplateView.as_view(template_name="core/aboutus.html"), name="home"),
-    path('contact-us/', TemplateView.as_view(template_name="core/contactus.html"), name="home"),
+    path('team/about-us/', TemplateView.as_view(template_name="core/about-us.html"), name="about_us"),
+    path('contact-us/', TemplateView.as_view(template_name="core/contact-us.html"), name="contact_us"),
+    path('contact-us/', TemplateView.as_view(template_name="core/work-sample.html"), name="work_sample"),
 ]
+
+# forms => fields => FormView => contact us 
