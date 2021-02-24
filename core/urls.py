b83c4f126e08e2c89fb4772120d12e96b0e9c03e
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-24 21:44:54 +0330

    Fix bug

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.views.generic import TemplateView
 
-
+app_name = "accounts"
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="core/home.html")),
+    path('', TemplateView.as_view(template_name="core/home.html"), name="home"),
 ]
