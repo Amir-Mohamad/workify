777bedcf6a6a9c60802f2b74de4e6f9ba93a96dd
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-25 22:35:09 +0330

    github

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class AboutUsModel(models.Model):
     name = models.CharField(max_length=100)
     bio = models.CharField(max_length=200)
+    github = models.CharField(max_length=500)
     image = models.ImageField()
     active = models.BooleanField(default=True)
 
