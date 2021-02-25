9ed0ec53951a2edd78af629bf8c30626b4e7b981
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-25 22:32:40 +0330

    bio - active

--- core/models.py
+++ core/models.py
@@ -3,3 +3,5 @@ from django.db import models
 
 class AboutUsModel(models.Model):
     name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
