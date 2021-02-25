e4695f429dd8688b03bfdcd07c56cd61aad3c18b
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-25 22:33:35 +0330

    Image Field

--- core/models.py
+++ core/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class AboutUsModel(models.Model):
     name = models.CharField(max_length=100)
     bio = models.CharField(max_length=200)
+    image = models.ImageField()
     active = models.BooleanField(default=True)
