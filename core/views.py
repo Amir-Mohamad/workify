149150e6daad7980c6ea3433dc11a289d54bb21b
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-25 23:12:09 +0330

    AboutUsList

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-
+from .models import AboutUsModel
 
 class AboutUsList(ListView):
+    queryset = AboutUsModel.objects.filter(active=True)
     template_name = 'core/about-us.html'
