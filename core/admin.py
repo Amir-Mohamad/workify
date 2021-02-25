3a7be52f6ecea7db82db47809d347759154e80d6
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-25 22:46:49 +0330

    Fix Bug

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . models import AboutUsModel
 
 class AboutUsAdmin(admin.ModelAdmin):
-    list_editable = ('name', 'is_active')
+    list_editable = ('name',)
     list_filter = ('name',)
 
 admin.site.register(AboutUsModel, AboutUsAdmin)
