fe8636b1e20c69b206a13cf4e4cf3cd33fbc9724
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-26 13:10:51 +0330

    Article - Category

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='articles')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     promote = models.BooleanField(default=False)
-    
+
+    def __str__(self):
+        return '{self.author} wrote {self.title} at {self.created}'
