079ec94215b2a767041cf8bb4666f0703e8151b1
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-26 13:03:13 +0330

    Fix Bug

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ArticleList.as_view(), name="article_list")
+]
--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace="core")),
-    path('', include('blog.urls', namespace="blog")),
+    path('blog/', include('blog.urls', namespace="blog")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
