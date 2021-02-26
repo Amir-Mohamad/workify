29556ed6b963c8b7dc66102bb8809a10f648faea
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-26 13:05:24 +0330

    author

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='articles')
