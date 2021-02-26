fe9aafb70131bbbafad825d91c7c00bc2f7bcee4
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-26 13:07:30 +0330

    ArticleModel

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,9 @@ from django.contrib.auth.models import User
 
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='articles')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    promote = models.BooleanField(default=False)
+    
