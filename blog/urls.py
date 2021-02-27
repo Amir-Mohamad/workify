b48fda0814ffcc33224187d176d0f08e77b719a3
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-02-27 23:01:38 +0330

    urls

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "blog"
 urlpatterns = [
     path('', views.ArticleList.as_view(), name="article_list")
+    path('<int:id>/<slug:slug>/', views.ArticleList.as_view(), name="article_list")
 ]
