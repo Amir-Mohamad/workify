ce05d1b398e7c3395ebb3964b3bc08e9c29b9f39
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-02-27 23:03:12 +0330

    Fix Path Bug

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = "blog"
 urlpatterns = [
-    path('', views.ArticleList.as_view(), name="article_list")
+    path('', views.ArticleList.as_view(), name="article_list"),
     path('<int:id>/<slug:slug>/', views.ArticleList.as_view(), name="article_list")
 ]
