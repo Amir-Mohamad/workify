465551f24107e520d1d45251a662019dc618f7d3
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-02-27 22:58:29 +0330

    imports

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from .models import Article, Category
+
 
 
 class ArticleList(ListView):
-    template_name = 'blog/article_list'
+    template_name = 'blog/article_list.html'
+
