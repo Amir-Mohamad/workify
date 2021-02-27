d1cf1aa5f15a081831cd428be5a5cb792d0e7a4e
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-02-27 22:54:13 +0330

    view

--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,3 @@ urlpatterns = [
 if settings.DEBUG:
     # urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# d
