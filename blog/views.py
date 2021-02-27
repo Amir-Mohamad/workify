cb1ee23f07d37e83e96ce7469458be5fb0e4bf4a
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-02-27 23:00:31 +0330

    Detail

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Article, Category
-
+from django.views.generic.detail import DetailView
 
 
 class ArticleList(ListView):
     template_name = 'blog/article_list.html'
     queryset = Article.objects.filter(is_active=True)
-
-
