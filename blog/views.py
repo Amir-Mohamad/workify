74922efafa4279fd9b54aed6f3417aae3da1046e
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-02-27 23:10:09 +0330

    Done(Im Not Sure)

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic.list import ListView
 from .models import Article, Category
 from django.views.generic.detail import DetailView
@@ -12,3 +12,5 @@ class ArticleDetail(DetailView):
     def get_queryset(self):
         id = self.kwargs['id']
         slug = self.kwargs['slug']
+        queryset = get_object_or_404(Article, id=id, slug=slug)
+        return queryset
