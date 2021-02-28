232cdbedc8ae26250d507320f8aafaeeffbcf96c
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-02-28 15:18:19 +0330

    CategoryList url

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.ArticleList.as_view(), name="article_list"),
     path('<int:id>/<slug:slug>/', views.ArticleDetail.as_view(), name="article_detail"),
     path('category/<slug:slug>/', views.CategoryList.as_view(), name="article_by_category"),
+	path('category/<slug:slug>/page/<int:page>', views.CategoryList.as_view(), name="category"),
     path('author/<slug:username>', views.AuthorList.as_view(), name="author"),
     path('author/<slug:username>/page/<int:page>', views.AuthorList.as_view(), name="author"),
 ]
