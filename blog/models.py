43af6b0dbc098fc119bdc90b16c3110e33b2c8c3
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-02 22:43:12 +0330

    Comment

--- blog/models.py
+++ blog/models.py
@@ -54,6 +54,21 @@ class Article(models.Model):
         return f'{self.author} wrote {self.title} at {self.created}'
 
 
+class Comment(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomment')
+	# post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pcomment')
+	# reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='rcomment')
+	# is_reply = models.BooleanField(default=False)
+	# body = models.TextField(max_length=400)
+	# created = models.DateTimeField(auto_now_add=True)
+
+	# def __str__(self):
+	# 	return f'{self.user} - {self.body[:30]}'
+
+	# class Meta:
+	# 	ordering = ('-created',)
+
+
 class Like(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='alike')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ulike')
