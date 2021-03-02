7c6383effdd95e47fdca14efdcb1d85ff27dae50
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-02 22:31:30 +0330

    None

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
