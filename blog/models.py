eaef1b0b24a3857bb7fa72aa30d229142d08e060
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-02 22:43:21 +0330

    post

--- blog/models.py
+++ blog/models.py
@@ -56,7 +56,7 @@ class Article(models.Model):
 
 class Comment(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomment')
-	# post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pcomment')
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pcomment')
 	# reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='rcomment')
 	# is_reply = models.BooleanField(default=False)
 	# body = models.TextField(max_length=400)
