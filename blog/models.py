c866b393a9c04bf4fbb8de07891a880a6901a10d
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-02 22:43:37 +0330

    reply and is_reply

--- blog/models.py
+++ blog/models.py
@@ -57,8 +57,8 @@ class Article(models.Model):
 class Comment(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomment')
 	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pcomment')
-	# reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='rcomment')
-	# is_reply = models.BooleanField(default=False)
+	reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='rcomment')
+	is_reply = models.BooleanField(default=False)
 	# body = models.TextField(max_length=400)
 	# created = models.DateTimeField(auto_now_add=True)
 
