3b4c1c48af0a564a66a646df9e3803a211922479
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-02 22:43:59 +0330

    body - created at

--- blog/models.py
+++ blog/models.py
@@ -59,8 +59,8 @@ class Comment(models.Model):
 	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pcomment')
 	reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='rcomment')
 	is_reply = models.BooleanField(default=False)
-	# body = models.TextField(max_length=400)
-	# created = models.DateTimeField(auto_now_add=True)
+	body = models.TextField(max_length=400)
+	created = models.DateTimeField(auto_now_add=True)
 
 	# def __str__(self):
 	# 	return f'{self.user} - {self.body[:30]}'
