8bfb842559612ff9333348d94837fabb59193dd3
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-02 22:41:36 +0330

    Comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Article, Category
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name','is_active')
     list_editable = ('is_active',)
