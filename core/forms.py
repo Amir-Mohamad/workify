b02046a3693b16a77995abcd521de5f37b3c2635
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-04 17:48:53 +0330

    ContactUsForm - Fix Bugs

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,16 @@
 from django import forms
+from django.db import models
 from django.db.models import fields
-from .models import NewsLetterModel
+from .models import ContactUsModel, NewsLetterModel
 
 
-class NewsLetterForm(forms.Form):
+class NewsLetterForm(forms.ModelForm):
     class Meta:
         model = NewsLetterModel
+        fields = '__all__'
+
+
+class ContactUsForm(forms.ModelForm):
+    class Meta:
+        model = ContactUsModel
         fields = '__all__'
--- core/views.py
+++ core/views.py
@@ -27,3 +27,4 @@ class AboutUsList(ListView):
     queryset = AboutUsModel.objects.filter(is_active=True)
     template_name = 'core/about-us.html'
     context_object_name = 'members'
+
