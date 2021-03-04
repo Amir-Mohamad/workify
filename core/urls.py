c2d7f37fbc95497766b46a0c17987a0ecbefcd82
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-04 17:26:06 +0330

    Home - urlsConfig

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 app_name = "core"
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="core/home.html"), name="home"),
+    path('', views.Home.as_view(), name="home"),
     path('team/about-us/', views.AboutUsList.as_view(), name="about_us"),
     path('contact-us/', TemplateView.as_view(template_name="core/contact-us.html"), name="contact_us"),
     path('team/work-sample/', TemplateView.as_view(template_name="core/work-sample.html"), name="work_sample"),
