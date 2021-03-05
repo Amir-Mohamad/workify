fb43ddc15dc57b264f8ad8a30810948fa14f4cf3
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-05 20:04:33 +0330

    pep8

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('accounts/', include('accounts.urls', namespace="accounts")),
     path('blog/', include('blog.urls', namespace="blog")),
@@ -10,6 +11,7 @@ urlpatterns = [
     path('', include('core.urls', namespace="core")),
     path('admin/', admin.site.urls),
 ]
+
 
 if settings.DEBUG:
     # urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
