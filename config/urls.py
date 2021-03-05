c851ea5185d15a69e070f54de581e2e003018341
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-05 20:00:03 +0330

    urls

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('accounts/', include('accounts.urls', namespace="accounts")),
     path('blog/', include('blog.urls', namespace="blog")),
+    path('learn/', include('learn.urls', namespace="learn")),
     path('', include('core.urls', namespace="core")),
     path('admin/', admin.site.urls),
 ]
