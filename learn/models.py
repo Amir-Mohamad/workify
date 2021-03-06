d74b6de13755803a1b2804e43de7524254ae05a0
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-06 13:40:06 +0330

    models - learn

--- learn/models.py
+++ learn/models.py
@@ -11,12 +11,14 @@ class Video(models.Model):
     def __str__(self):
         return 
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
 
 class Course(models.Model):
     teacher = models.ForeignKey(User, on_delete=models.CASCADE)
