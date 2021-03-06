b27bcd4c7bb2af3d674add962fa51fbfa5de8613
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-06 11:20:33 +0330

    views - learn

--- learn/views.py
+++ learn/views.py
@@ -1,2 +1,8 @@
+from typing import List
 from django.shortcuts import render
+from django.views.generic import ListView
 
+
+class CourseListView(ListView):
+    queryset = Course.objects.filter(is_active=True)
+    template_name = 'learn/course_list.html'
