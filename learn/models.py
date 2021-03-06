1d0aa61a44c76f016f3e844d933f2f641c8cf4f2
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-06 13:34:40 +0330

    models - learn

--- learn/models.py
+++ learn/models.py
@@ -6,7 +6,6 @@ class Course(models.Model):
     teacher = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    
     short_description = models.CharField(max_length=500)
     description = models.TextField()
     cover = models.ImageField()
@@ -21,3 +20,7 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
+class Video(models.Model):
+    youtube = models.CharField(max_length=500)
+    aparat = models.CharField(max_length=500)
