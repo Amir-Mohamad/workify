fb1c1c2f98f250c350cfcf277b04df63851a457d
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-06 13:44:37 +0330

    Remove learn app

--- learn/views.py
+++ learn/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 
-
 # Showing the list of availabe courses
 class CourseListView(ListView):
     queryset = Course.objects.filter(is_active=True)
