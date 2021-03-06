61d51e120f1560cb9dda9c76c31b523be54a33af
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-06 13:37:59 +0330

    models - learn

--- learn/models.py
+++ learn/models.py
@@ -2,15 +2,22 @@ from django.db import models
 from accounts.models import User
 
 
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    youtube = models.CharField(max_length=500)
+    aparat = models.CharField(max_length=500)
+
+    def __str__(self):
+        return 
+        
 class Course(models.Model):
     teacher = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    videos = models.ForeignKey(Video, on_delete=models.CASCADE)
     short_description = models.CharField(max_length=500)
-    description = models.TextField()
     cover = models.ImageField()
-    youtube = models.CharField(max_length=500) # Youtube
-    aparat = models.CharField(max_length=500) # Aparat
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     price = models.IntegerField() # I dont now the parameters
@@ -20,12 +27,3 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
-
-class Video(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    youtube = models.CharField(max_length=500)
-    aparat = models.CharField(max_length=500)
-
-    def __str__(self):
-        return 
