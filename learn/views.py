3a35e515fe76bc075e0b563068344889d04ffee3
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-06 13:43:32 +0330

    Remove learn app

--- learn/views.py
+++ learn/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 
-# class CourseListView(ListView):
-#     queryset = Course.objects.filter(is_active=True)
-#     template_name = 'learn/course_list.html'
+
+class CourseListView(ListView):
+    queryset = Course.objects.filter(is_active=True)
+    template_name = 'learn/course_list.html'
