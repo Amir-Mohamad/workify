cfbb58563d47e00b5423aa12b85e7959260479fe
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-06 11:33:34 +0330

    models - learn

--- learn/models.py
+++ learn/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Course(models.Model):
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    short_description = models.CharField(max_length=500)
+    description = models.TextField()
+    video = models.FileField() # I DONT KNOW 
+    is_active = models.BooleanField(default=True)
+    is_special = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
