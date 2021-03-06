55490c5814952abd1a6d791451dbde4e9604478e
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-06 13:41:00 +0330

    Remove learn app

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('accounts/', include('accounts.urls', namespace="accounts")),
     path('blog/', include('blog.urls', namespace="blog")),
-    path('learn/', include('learn.urls', namespace="learn")),
+    # path('learn/', include('learn.urls', namespace="learn")),
     path('', include('core.urls', namespace="core")),
     path('admin/', admin.site.urls),
 ]
