001910381bddb261b72af4fdbf3e10f403514631
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-07 22:58:00 +0330

    CBV contactus - clean

--- core/views.py
+++ core/views.py
@@ -50,8 +50,6 @@ class AboutUsList(ListView):
 #             return redirect('core:home')
 #     return render(request, 'core/contact-us.html', {'form':form})
 
-
-
 class ContactUs(LoginRequiredMixin, message, CreateView):
     template_name = 'core/contact-us.html'
     form_class = ContactUsForm
