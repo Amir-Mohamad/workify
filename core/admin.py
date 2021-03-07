7f6d17f1482ad8fd8e195438d37bc3307bf16289
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-07 23:01:09 +0330

    CBV contactus - clean

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from . models import AboutUsModel, ContactUsModel
 
+
 class AboutUsAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_active')
     list_editable = ('is_active',)
     list_filter = ('name',)
+
 
 admin.site.register(AboutUsModel, AboutUsAdmin)
 admin.site.register(ContactUsModel)
