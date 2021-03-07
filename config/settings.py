24149fbb8f9cb0d4df53c27c831621baaaf0238b
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-07 23:16:32 +0330

    Clean

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,6 @@ DATABASES = {
 }
 
 
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -83,6 +82,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 AUTH_USER_MODEL = 'accounts.User'
 # AUTHENTICATION_BACKENDS = [
