6578d7f45e996d937d6b1b3672028e075b20c4bf
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-11 00:49:32 +0330

    work-samples - order.html

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import AboutUsModel, ContactUsModel
+from . models import AboutUsModel, ContactUsModel, WorkSamples
 
 
 class AboutUsAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ class AboutUsAdmin(admin.ModelAdmin):
 
 admin.site.register(AboutUsModel, AboutUsAdmin)
 admin.site.register(ContactUsModel)
+admin.site.register(WorkSamples)
