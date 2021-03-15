02a3ebc3f7485d2bd81c186c6ab2eef12eb96138
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-16 01:01:04 +0330

    test: test_contact_us_page_

--- core/tests/test_urls.py
+++ core/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
-from core.views import Home
+from core.views import ContactUs, Home
 
 
 class TestUrls(SimpleTestCase):
@@ -8,3 +8,7 @@ class TestUrls(SimpleTestCase):
     def test_home_page_view(self):
         url = reverse('core:home')
         self.assertEqual(resolve(url).func.view_class, Home)
+
+    def test_contact_us_page_(self):
+        url = reverse('core:contact_us')
+        self.assertEqual(resolve(url).func.view_class, ContactUs)
