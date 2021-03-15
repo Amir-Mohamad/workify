72d2658c8529b437b54c07349955de1b7ca42f0d
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-15 23:44:56 +0330

    CleanCode - AboutUsModel

--- core/models.py
+++ core/models.py
@@ -7,6 +7,7 @@ class AboutUsModel(models.Model):
     name = models.CharField(max_length=100)
     bio = models.CharField(max_length=200)
     github = models.CharField(max_length=500)
+    instagram = models.CharField(max_length=500)
     image = models.ImageField()
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
