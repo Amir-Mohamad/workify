df89a56d9f4d8d66b587d5a1a1b1831085a757a3
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-15 23:27:52 +0330

    CleanCode

--- accounts/views.py
+++ accounts/views.py
@@ -43,6 +43,7 @@ def UserRegister(request):
 		return redirect('accounts:verify')
 	return render(request, 'accounts/register.html', {'form':form})
 
+
 def VerifyCode(request):
 	form = VerifyForm(request.POST or None)
 	if form.is_valid():
