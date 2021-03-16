40699914997cc7cd238664ebda71fbb0d1dfb786
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-16 21:29:12 +0330

    tests: test_about_us_page_response

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -11,3 +11,7 @@ class TestViews(TestCase):
     def test_home_page_view_response(self):
         response = self.client.get(reverse('core:home'))
         self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_response(self):
+        response = self.client.get(reverse('core:about_us'))
+        
