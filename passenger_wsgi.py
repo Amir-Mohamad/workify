5a92cb3407e0f8be8791401c67eebea870ad49ab
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-17 19:36:53 +0330

    Ready To Deploy

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,12 @@
+
+
+import imp
+
+
 import os
 import sys
 
 
-sys.path.insert(0, os.path.dirname(__file__))
-
-
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    response = '\n'.join([message, version])
-    return [response.encode()]
+sys.path.insert(۰, os.path.dirname(__file__))
+wsgi = imp.load_source('wsgi', 'config/wsgi.py')
+application = wsgi.application
