3a4f9ee6864a445be492acd555fa43fc6550863d
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-18 18:15:41 +0330

    Profile bug - email bugs

--- config/settings.py
+++ config/settings.py
@@ -113,11 +113,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/home/megacod2/public_html/static/'
+STATIC_ROOT = '/home/megacod2/public_html/static/'
 STATICFILES_DIRS = [BASE_DIR, "static"]
 
 
-# MEDIA_ROOT = '/home/megacod2/public_html/media/'
+MEDIA_ROOT = '/home/megacod2/public_html/media/'
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
