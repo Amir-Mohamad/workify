6705f0c0a5b5352a27506e2500329e796670075b
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-19 23:49:41 +0330

    test_register_page_GET method

--- accounts/tests/test_urls.py
+++ accounts/tests/test_urls.py
@@ -1,7 +1,17 @@
+from django.http import response
 from django.test import TestCase
 from django.test.client import Client
+from django.urls import reverse, resolve
 
 
 class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
+
+    def test_login_page_GET(self):
+        response = self.client.get(reverse('accounts:login'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_register_page_GET(self):
+        response = self.client.get(reverse('accounts:register'))
+        self.assertEqual(response.status_code, 200)
