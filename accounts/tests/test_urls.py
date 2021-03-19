9dc8701cd8ab311093430627e3cf082ef3e3f952
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-19 23:54:14 +0330

    testing urls

--- accounts/tests/test_urls.py
+++ accounts/tests/test_urls.py
@@ -2,12 +2,14 @@ from django.http import response
 from django.test import TestCase
 from django.test.client import Client
 from django.urls import reverse, resolve
+from ..views import UserLogin
 
 
 class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
 
+    # testing status codes
     def test_login_page_GET(self):
         response = self.client.get(reverse('accounts:login'))
         self.assertEqual(response.status_code, 200)
@@ -15,3 +17,7 @@ class TestViews(TestCase):
     def test_register_page_GET(self):
         response = self.client.get(reverse('accounts:register'))
         self.assertEqual(response.status_code, 200)
+
+    def test_login_view(self):
+        url = reverse('accounts:login')
+        self.assertEqual(resolve(url).func.view_class, UserLogin)
