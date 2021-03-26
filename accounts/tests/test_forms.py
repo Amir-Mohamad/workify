1a858d1002b922381375ac84bb2f3137e4c20ce2
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-03-26 21:39:52 +0430

    test: TestRegistrationForm

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -4,14 +4,25 @@ from ..models import User
 
 class TestRegisterForm(TestCase):
 
-    def test_register_form(self):
+    def test_register_form_used_before(self):
         User.objects.create(
             email="jack@gmail.com",
             password="jackpass1384",
         )
         form = RegisterForm(data={
             "email":"jack@gmail.com",
-            'password':"jackpass1384",
+            'password1':"jackpass1384",
+            'password2':"jackpass1384",
+        })
+
+        # we will get False because that email was created before
+        self.assertEqual(bool(form.is_valid()), False)
+
+    def test_password_not_match(self):
+        form = RegisterForm(data={
+            'email':'amir@gmail.com',
+            'password1':'Amirtest1897',
+            'password1':'Amirtest1898',
         })
 
         self.assertEqual(bool(form.is_valid()), False)
