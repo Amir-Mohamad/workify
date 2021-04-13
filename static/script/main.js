00d5eb5deefe2f20bed399f5f0a00e245dcc246d
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-04-13 22:30:14 +0430

    clean: main.js

--- static/script/main.js
+++ static/script/main.js
@@ -3,6 +3,7 @@ const header = document.querySelector('#main-header');
 const headerFixed = document.querySelector('.header-fixed');
 const menuButton = document.querySelector('#menu-button');
 
+
 // menu bar
 const toggleMenu = () => {
   header.classList.toggle('collapsed');
@@ -13,6 +14,7 @@ const fixedNavbar = () => {
   headerFixed.classList.toggle('sticky', window.scrollY > 100);
 };
 
+
 if (menuButton) menuButton.addEventListener('click', toggleMenu);
 if (headerFixed) window.addEventListener('scroll', fixedNavbar);
 // ===================================================
@@ -21,10 +23,12 @@ const passwordIcon = document.querySelector('.password-icon');
 const confirmPasswordInput = document.querySelector('#confirm-password');
 const confirmPasswordIcon = document.querySelector('.confirm-password-icon');
 
+
 const eyeIcon =
   '<svg class="eye" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>';
 const eyeOffIcon =
   '<svg class="eye-off" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path></svg>';
+
 
 const togglePassword = (input, icon) => {
   if (input.type === 'password') {
@@ -36,6 +40,7 @@ const togglePassword = (input, icon) => {
   }
 };
 
+
 try {
   passwordIcon.addEventListener('click', () =>
     togglePassword(passwordInput, passwordIcon)
@@ -46,10 +51,13 @@ try {
 } catch {
   null;
 }
+
 // ===================================================
 // navbar
+
 var headerNav = document.querySelector('#main-header');
 
+// Fix nabar bug
 if(window.location.pathname !== '/'){
   headerNav.classList.add('main-header-bg');
 }
