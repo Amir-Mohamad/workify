7cfc7ea1e6312676d7037de0133f298fc2a0a857
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-04-17 22:52:27 +0430

    clean: pep8

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from .models import Article, Category
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','is_active')
+    list_display = ('name', 'is_active')
     list_editable = ('is_active',)
     list_filter = ('name',)
     prepopulated_fields = {'slug': ('name',)}
@@ -12,8 +12,9 @@ class CategoryAdmin(admin.ModelAdmin):
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_active', 'promote')
     list_filter = ('is_active', 'title', 'promote')
-    list_editable = ('is_active','promote')
+    list_editable = ('is_active', 'promote')
     prepopulated_fields = {'slug': ('title',)}
+
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category, CategoryAdmin)
