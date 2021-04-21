e27762c76cb9cee8da8e7b72db0ca99e14941a95
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-04-21 23:58:30 +0430

    clean: pep8

--- core/tests/test_forms.py
+++ core/tests/test_forms.py
@@ -6,12 +6,12 @@ from ..forms import ContactUsForm, NewsLetterForm, OrderForm
 class TestContactForm(SimpleTestCase):
     def test_valid_data(self):
         form = ContactUsForm(data={
-            'fullname':"amirmohamad", 
-            'title':"title", 
-            'description':"description"}
+            'fullname': "amirmohamad",
+            'title': "title",
+            'description': "description"}
         )
         self.assertTrue(form.is_valid())
-    
+
     def test_invalid_form(self):
         form = ContactUsForm(data={})
         self.assertEqual(len(form.errors), 3)
@@ -21,9 +21,9 @@ class TestContactForm(SimpleTestCase):
 class TestOrderForm(SimpleTestCase):
     def test_valid_data(self):
         form = OrderForm(data={
-            'title':'title',
-            'description':'description',
-            'phone':'09104958451',
+            'title': 'title',
+            'description': 'description',
+            'phone': '09104958451',
         })
         self.assertTrue(form.is_valid())
 
@@ -36,8 +36,8 @@ class TestOrderForm(SimpleTestCase):
 class TestNewModel(SimpleTestCase):
     def test_valid_data(self):
         form = NewsLetterForm(data={
-            'email':'jack@gmail.com',
-            'phone':'09104958451',
+            'email': 'jack@gmail.com',
+            'phone': '09104958451',
         })
         self.assertTrue(form.is_valid())
 
